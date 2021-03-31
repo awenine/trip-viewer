7426bdb51ee25230fdbfb775a39f9be4a527f9d2
Chris Owen <c.ornine@gmail.com>
2021-03-31 13:30:40 +0100

    feat(apiclient.ts): add and type API services

---
+++ src/services/APIClient.ts
@@ -0,0 +1,23 @@
+import { Location, LocationDetails } from "../types"
+
+const BASE_URL = process.env.REACT_APP_FETCH_URL;
+
+export const getLocations = (searchTerm: string): Promise<Location> => {
+  return fetch(`${BASE_URL}?q=${searchTerm}`)
+    .then((res) => (res.status <= 400 ? res : Promise.reject(res)))
+    .then((res) => res.json())
+    .catch((err) => {
+      console.log(`${err.message} while fetching location`);
+    });
+};
+
+export const getLocationDetails = (locationID: number): Promise<LocationDetails> => {
+  return fetch(`${BASE_URL}/${locationID}`)
+    .then((res) => (res.status <= 400 ? res : Promise.reject(res)))
+    .then((res) => res.json())
+    .catch((err) => {
+      console.log(`${err.message} while fetching location with id ${locationID}`);
+    });
+};
+
+// export {}
